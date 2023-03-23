010bd5d3a8826203c6515d855ed7cbb5cd99f208
anshulg614 <122504590+anshulg614@users.noreply.github.com>
2023-03-23 18:45:00 -0400

    Pretty much finished application, good express.js practice

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,47 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+function isValidPost(post) {
+  return post.messageUser && post.messageUser.toString().trim() !== '' && post.messageText && post.messageText.toString().trim() !== '';
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date()
-    }
- ];
-  
- res.render('index', { title: "Mini Messageboard", messages: messages });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
+
+/* GET on /new - display message form to client */
+router.get('/new', function(req, res, next) {
+  res.render('forms')
+})
+
+/* POST message */
+router.post('/new', function(req, res, next) {
+
+  // validation - need to add an else statement (hey! please enter a name and message)
+  if (isValidPost(req.body)) {
+    // add new message to the array
+    messages.push({
+      text: req.body.messageText.toString().trim(),
+      user: req.body.messageUser.toString().trim(),
+      added: new Date()
+    })
+  }
+  // send user back to homepage
+  res.redirect('/')
+})
 
 module.exports = router;
